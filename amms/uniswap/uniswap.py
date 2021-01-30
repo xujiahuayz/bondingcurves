d72eec351f048144c132a4d77c36da41506936c3
naz <nazariy@inbox.ru>
2021-01-30 19:01:15 +0000

    [+] trade function

--- amms/uniswap/uniswap.py
+++ amms/uniswap/uniswap.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # external jazz
 from enum import Enum
+from typing import Any
 
 # workspace modules
-from amms.logger import l
-from amms.uniswap.utils import Token, LogHelper
+from amms.uniswap.utils import Token, LogHelper as l, quote
 
 MUST_SUPPLY_EQUAL_VALUES = Exception("must supply equal values")
 
@@ -24,9 +24,9 @@ class Amm:
     # @param x_1 - qty of coin 1 to deposit
     # @param x_2 - qty of coin 2 to deposit
     def __init__(self, x_1: Token, x_2: Token):
-        if not x_1.i == 1:
+        if x_1.name != "x_1":
             return Exception("must be 1")
-        if not x_2.i == 2:
+        if x_2.name != "x_2":
             return Exception("must be 2")
 
         self.x_1 = x_1.qty
@@ -38,44 +38,58 @@ class Amm:
         # x_1 qty of token 1 and x_2 qty of token 2
         # gets transfered from the pool cretor into the pool (called a pair in uniswap contracts)
 
-        LogHelper.pool_created(self.x_1, self.x_2, self.invariant)
+        l.pool_created(self.x_1, self.x_2, self.invariant)
 
     def add_liquidity(self, x_i: Token):
-        prev_x_1, prev_x_2 = self.x_1, self.x_2
-        prev_invariant = self.invariant
+        prev_x_1, prev_x_2, prev_invariant = self.x_1, self.x_2, self.invariant
 
-        self.__setattr__(x_i.name, self.__getattribute__(x_i.name) + x_i.qty)
-        self.__setattr__(
-            x_i.complement, self.invariant / self.__getattribute__(x_i.name)
-        )
+        self._set(x_i.name, self._get(x_i.name) + x_i.qty)
+        x_j = quote(x_i.qty, self._get(x_i.name), self._get(x_i.complement))
+        self._set(x_i.complement, self._get(x_i.complement) + x_j)
         self.invariant = self.x_1 * self.x_2
 
-        LogHelper.added_liquidity(
-            prev_x_1, self.x_1, prev_x_2, self.x_2, prev_invariant, self.invariant
+        l.added_liquidity(
+            self.x_1, prev_x_1, self.x_2, prev_x_2, self.invariant, prev_invariant
         )
 
-    # trading fees
-    # def _mint_fee(self, new_invariant: float):
+    def trade(self, x_i: Token):
+        prev_x_1, prev_x_2, prev_invariant = self.x_1, self.x_2, self.invariant
 
-    # def remove_liquidity(self, x_i: Token):
+        x_j = quote(
+            x_i.qty,
+            self._get(x_i.name),
+            self._get(x_i.complement),
+        )
 
-    # my x_1 gets sent into the pool, I get back x_2
-    # x_1 \in R^+ and x_1 > 0
-    # def trade(self, x_i: Token):
-    # if x_1 <= 0:
-    #     return
+        self._set(x_i.name, self._get(x_i.name) + x_i.qty)
+        self._set(x_i.complement, self._get(x_i.complement) - x_j)
 
-    # old_ex_rate = self.x_2 / self.x_1
+        l.trade_executed(
+            x_i,
+            x_j,
+            self.x_1,
+            prev_x_1,
+            self.x_2,
+            prev_x_2,
+            self.invariant,
+            prev_invariant,
+        )
 
-    # self.x_1 += x_i_qty
-    # # if the divisor was original x_1, we would get x_2
-    # new_x_2 = self.invariant / self.x_1
-    # x_2_to_send = self.x_2 - new_x_2
-    # self.x_2 = new_x_2
+        return x_j
 
-    # new_ex_rate = self.x_2 / self.x_1
-    # # trader x_1 -> uniswap
-    # # uniswap x_2 -> trader
-    # l.info(
-    #     f"swap x_1 for x_2. uniswap r'cvd x_1 = +${x_i_qty}, trader r'cvd x_2 = +${x_2_to_send:.8f}. old ex. rate x1/x2 ${old_ex_rate:.8f}. new ex.rate x1/x2 ${new_ex_rate:.8f}"
-    # )
+    def _get(self, name: str):
+        return self.__getattribute__(name)
+
+    def _set(self, name: str, value: Any):
+        self.__setattr__(name, value)
+
+
+if __name__ == "__main__":
+    x_1 = Token(1, 100)
+    x_2 = Token(2, 50)
+    amm = Amm(x_1, x_2)
+
+    amm.add_liquidity(Token(2, 30))
+    amm.trade(Token(1, 55))
+    amm.trade(Token(1, 23))
+    amm.trade(Token(1, 100))
--- amms/uniswap/utils.py
+++ amms/uniswap/utils.py
@@ -13,27 +13,53 @@ class Token:
 
         self.name = f"x_{x_i}"
         self.qty = x_i_qty
-        self.complement = f"x_{2 if x_i is 1 else 1}"
+        self.complement = f"x_{2 if x_i == 1 else 1}"
+
+    def __repr__(self):
+        return f"Token(x_i={self.name}, x_i_qty={self.qty})"
 
 
 class LogHelper:
     @staticmethod
     def pool_created(x_1, x_2, invariant):
         l.info(
-            f"CREATED POOL.\n"
+            f"\nCREATED POOL.\n"
             f"x_1, x_2: {x_1:.8f}, {x_2:.8f}.\n"
             f"invariant {invariant}.\n"
             f"ex. rate x_1/x_2 = {x_2/x_1}.\n"
-            f"{DIVIDER}\n\n"
+            f"{DIVIDER}\n"
+        )
+
+    @staticmethod
+    def trade_executed(
+        x_i: Token, x_j_qty, x_1, prev_x_1, x_2, prev_x_2, invariant, prev_invariant
+    ):
+        l.info(
+            "\nEXECUTED TRADE.\n"
+            f"swapped {x_i.qty} {x_i.name} for {x_j_qty} {x_i.complement}\n"
+            f"prev ex.rate x_1/x_2 = {prev_x_2/prev_x_1:.8f}.\n"
+            f"prev x_1, prev x_2: {prev_x_1:.8f}, {prev_x_2:.8f}\n"
+            f"prev invarinat {prev_invariant}\n"
+            f"ex. rate x_1/x_2 = {x_2/x_1:.8f}.\n"
+            f"x_1, x_2: {x_1:.8f}, {x_2:.8f}.\n"
+            f"invariant {invariant}.\n"
+            f"{DIVIDER}\n"
         )
 
     @staticmethod
     def added_liquidity(x_1, prev_x_1, x_2, prev_x_2, invariant, prev_invariant):
         l.info(
-            f"ADDED LIQUIDITY.\n"
+            f"\nADDED LIQUIDITY.\n"
             f"Δx_1, Δx_2: +{(x_1 - prev_x_1):.8f}, +{(x_2 - prev_x_2):.8f}.\n"
             f"x_1, x_2: {x_1:.8f}, {x_2:.8f}.\n"
-            f"prev. invariant: ${prev_invariant:.8f}.\n"
-            f"invariant: ${invariant:.8f}.\n"
-            f"{DIVIDER}\n\n"
+            f"prev. invariant: {prev_invariant:.8f}.\n"
+            f"invariant: {invariant:.8f}.\n"
+            f"{DIVIDER}\n"
         )
+
+
+# x_i_qty is the user sent delta
+def quote(x_i_qty: float, x_i_reserve: float, x_j_reserve: float):
+    x_js_for_one_i = x_j_reserve / x_i_reserve  # this is your ex rate: x_i / x_j
+    x_j_qty = x_i_qty * x_js_for_one_i
+    return x_j_qty
