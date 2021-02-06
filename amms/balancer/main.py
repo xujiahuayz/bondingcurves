a2ae411ff5521955878cdf2a40974b46135dbb4d
nazariyv <nazariy@inbox.ru>
2021-02-06 22:58:41 +0000

    [*] balancer not bancor

--- amms/balancer/main.py
+++ amms/balancer/main.py
@@ -10,4 +10,4 @@ module_path = os.path.dirname(os.path.dirname(os.path.abspath(".")))
 
 # to make it work out of the box in interactive shells
 if module_path not in sys.path:
-  sys.path.insert(0, module_path)
+    sys.path.insert(0, module_path)
