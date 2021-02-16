ab2f903fe04b9be7a2078879062549647e96fdf4
xujiahuayz <31636867+xujiahuayz@users.noreply.github.com>
2021-02-16 23:15:31 +0000

    update curve

---
+++ amms/curve/mathexpressions.py
@@ -0,0 +1,87 @@
+import sympy as sp
+from sympy.parsing import mathematica as M
+from sympy.parsing import sympy_parser as sparser
+from math import sqrt
+
+express2 = """
+(-2 6 ^ (2/3)(-1 + a) proall + 6 ^ (1/3)(proall(9 a sumall + Sqrt[48 (-1 + a) ^ 3 proall + 81 a ^ 2 sumall ^ 2])) ^ (2/3))/(6 (proall(9 a sumall + Sqrt[48 (-1 + a) ^ 3 proall + 81 a ^ 2 sumall ^ 2])) ^ (1/3))
+"""
+
+express3 = """
+(Sqrt[-8 a proall sumall + 
+     2^(1/3) (27 (-1 + a)^2 proall^2 + Sqrt[
+        proall^3 (729 (-1 + a)^4 proall + 256 a^3 sumall^3)])^(
+      2/3)] - \[Sqrt](8 a proall sumall - 
+       2^(1/3) (27 (-1 + a)^2 proall^2 + Sqrt[
+          proall^3 (729 (-1 + a)^4 proall + 256 a^3 sumall^3)])^(
+        2/3) - (12 Sqrt[3] (-1 + a) proall)/
+       Sqrt[(-8 a proall sumall + 
+        2^(1/3) (27 (-1 + a)^2 proall^2 + Sqrt[
+           proall^3 (729 (-1 + a)^4 proall + 256 a^3 sumall^3)])^(
+         2/3))/(27 (-1 + a)^2 proall^2 + Sqrt[
+        proall^3 (729 (-1 + a)^4 proall + 
+           256 a^3 sumall^3)])]))/(2 2^(1/3) Sqrt[
+    3] (27 (-1 + a)^2 proall^2 + Sqrt[
+      proall^3 (729 (-1 + a)^4 proall + 256 a^3 sumall^3)])^(1/6))
+"""
+
+
+# q function when n=2
+def normalized_qty2(sumall, proall, a):
+    q = (-2*6**(2/3)*proall*(a - 1) + 6**(1/3)*(proall*(9*a*sumall + sqrt(81*a**2*sumall**2 + 48*proall*(a - 1)**3)))
+         ** (2/3))/(6*(proall*(9*a*sumall + sqrt(81*a**2*sumall**2 + 48*proall*(a - 1)**3)))**(1/3))
+    return q
+
+
+# q function when n=2
+def normalized_qty3(sumall, proall, a):
+    q = (sqrt(
+        -8*a*proall*sumall+2**(1/3)*(27*(-1+a)**2*proall**2+sqrt(proall **
+                                                                 3*(729*(-1+a)**4*proall+256*a**3*sumall**3)))**(2/3)
+    ) - sqrt(
+        8*a*proall*sumall-2**(1/3)*(
+            27*(-1+a)**2*proall**2+sqrt(proall**3 *
+                                        (729*(-1+a)**4*proall+256*a**3*sumall**3))
+        )**(2/3)-(
+            12*sqrt(3)*(-1+a)*proall)/sqrt(
+                (-8*a*proall*sumall + 2**(1/3)*(27*(-1+a)**2*proall**2+sqrt(proall**3*(729*(-1+a)**4*proall+256*a**3*sumall**3)))
+                 ** (2/3))/(27*(-1+a)**2*proall**2+sqrt(proall**3*(729*(-1+a)**4*proall+256*a**3*sumall**3)))
+        )))/(
+        2*2**(1/3)*sqrt(3)*(27*(-1+a)**2*proall**2+sqrt(proall **
+                                                        3*(729*(-1+a)**4*proall+256*a**3*sumall**3)))**(1/6)
+    )
+    return q
+
+
+if __name__ == "__main__":
+
+    expr2 = M.mathematica(express2)
+    print(expr2)
+
+    # Didn't work perfectly for some reason
+    expr3 = M.mathematica(express3)
+    print(expr3)
+
+    # try equal size, each asset with quantity x
+    def equal_size_q_2(x, a=15):
+        q = normalized_qty2(sumall=x*2, proall=x**2, a=a)
+        return q
+
+    # should equal to input x
+    equal_size_q_2(8)
+
+    # try equal size, each asset with quantity x, check q = x
+    def check_equal_size_q_2(x, a=15):
+        q = normalized_qty2(sumall=x*2, proall=x**2, a=a)
+        good_q = x
+        return good_q-q
+
+        # try a=1, check q = (proall * sumall / 2)**(1/2)
+    def check_a1_2(sumall, proall):
+        q = normalized_qty2(sumall=sumall, proall=proall, a=1)
+        good_q = (proall * sumall/2)**(1/3)
+        return good_q-q
+
+    # should equal to input x
+    check_equal_size_q_2(10) < 1e-10
+    check_a1_2(34, 24) < 1e-10
--- amms/curve/playground.py
+++ amms/curve/playground.py
@@ -4,29 +4,86 @@ from math import prod, sqrt
 
 
 class curvefi:
-    def __init__(self, coin_qty, n, a):
+    def __init__(self, *args, a):
 
-        self.reserve = [coin_qty] * n
+        self.reserve = list(args)
 
         # number of token sorts in the pool
-        self.n = n
+        self.n = len(args)
 
         self.a_inv = a
 
-        # transform our aInv into StableSwap's A (not used in this class)
-        self.A = a / (n ** n)
+        coin_qty = self.normalized_qty()
 
         # sum invariant
-        self.sum_inv = coin_qty * n
+        self.sum_inv = coin_qty * self.n
 
         # product invariant
-        self.prod_inv = coin_qty ** n
+        self.prod_inv = coin_qty ** self.n
+
+        # Initial pool share set the same as normalized total coin qty
+        self.totalshares = self.sum_inv
+
+        # # transform our aInv into StableSwap's A (not used in this class)
+        # self.A = a / (n ** n)
+
+        # # sum invariant
+        # self.sum_inv = coin_qty * n
+
+        # # product invariant
+        # self.prod_inv = coin_qty ** n
 
     def poolsum(self):
         return sum(self.reserve)
 
     def poolprod(self):
         return prod(self.reserve)
+
+    def normalized_qty(self):
+
+        # Special case with qual size pool, no need to calculate, although results are the same
+        if len(set(self.reserve)) == 1:
+            q = self.reserve[0]
+        else:
+            sumall = self.poolsum()
+            proall = self.poolprod()
+            n = self.n
+            a = self.a_inv
+
+            # Special case with a=0 or 1, no need to calculate, although results are the same
+            if a < 1e-10:
+                q = proall ** (1/n)
+
+            elif a == 1:
+                q = (proall*sumall/n)**(1/(n+1))
+
+            elif n == 2:
+                q = (-2*6**(2/3)*proall*(a - 1) + 6**(1/3)*(proall*(9*a*sumall + sqrt(81*a**2*sumall**2 + 48*proall*(a - 1)**3)))
+                     ** (2/3))/(6*(proall*(9*a*sumall + sqrt(81*a**2*sumall**2 + 48*proall*(a - 1)**3)))**(1/3))
+            else:
+                raise Exception('Cannot handle unequal asset pool with n>2')
+        return q
+
+    def add_liquidity(self, addamount, addindex: int):
+        # update reserve
+        self.reserve[addindex] += addamount
+
+        # calculate new normalized qty
+        coin_qty = self.normalized_qty()
+
+        # new sum invariant
+        sum_inv_new = coin_qty * self.n
+
+        # newly issued shares are the difference between new and old sum_inv
+        newshares = sum_inv_new - self.sum_inv
+
+        # update sum invariant
+        self.sum_inv = sum_inv_new
+
+        # update product invariant
+        self.prod_inv = coin_qty ** self.n
+
+        return newshares
 
     def exchange(self, inamount, inindex: int, outindex: int):
         a = self.a_inv
@@ -45,9 +102,9 @@ class curvefi:
         ) * inres
 
         outres = (
-            (1 - 1 / a) * D - sumexo +
-            sqrt(((1 - 1 / a) * D - sumexo) ** 2 + 4 * D * X / a / prodexo)
-        ) / 2
+            (1 - 1 / a) * D - sumexo + sqrt(
+                ((1 - 1 / a) * D - sumexo) ** 2 + 4 * D * X / a / prodexo
+            )) / 2
 
         outamount = self.reserve[outindex] - outres
 
@@ -60,15 +117,14 @@ class curvefi:
         return outamount
 
 
-def inout(coin_qty, n, inamount, inindex: int = 0, outindex: int = 1, a=80):
-    demoPool = curvefi(coin_qty, n, a=a)
+def inout(*args, inamount, inindex: int = 0, outindex: int = 1, a=80):
+    demoPool = curvefi(*args, a=a)
     demoPool.exchange(inamount=inamount, inindex=inindex, outindex=outindex)
     return demoPool.reserve[inindex], demoPool.reserve[outindex]
 
 
 def plotinout(
-    coin_qty,
-    n,
+    *args,
     inrange=np.arange(-299, 699),
     inindex=0,
     outindex=1,
@@ -76,8 +132,7 @@ def plotinout(
 ):
     reservepair = [
         inout(
-            coin_qty,
-            n,
+            *args,
             inamount=x,
             inindex=0,
             outindex=outindex,
@@ -92,13 +147,19 @@ def plotinout(
 
 
 if __name__ == "__main__":
-    oneassetreserve = 300
-    n = 6
+    qty1 = 300
+    qty2 = 400
+    # n = 6
 
     for a in [1e-12, 1, 10, 1e20]:
-        plotinout(oneassetreserve, n, a=a)
-    plt.scatter(oneassetreserve, oneassetreserve, label="inital state")
+        plotinout(qty1, qty2, a=a)
+    plt.scatter(qty1, qty2, label="inital state")
     plt.xlabel('$R_1$ quantity')
     plt.ylabel('$R_2$ quantity')
     plt.legend()
     plt.show()
+
+    newpool = curvefi(9000, 11000, a=500)
+    newpool.sum_inv
+
+    newpool.add_liquidity(800, 0)
