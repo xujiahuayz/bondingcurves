4197ad9aa1037b445fa294afdad627f4c992187a
nazariyv <nazariy@inbox.ru>
2021-02-16 01:29:34 +0000

    [*] minor code mod

--- amms/curve/playground.py
+++ amms/curve/playground.py
@@ -1,32 +1,28 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from math import prod, sqrt
 
 
 class curvefi:
-    def __init__(self, *args, a):
+    def __init__(self, coin_qty, n, a):
 
-        self.reserve = list(args)
+        self.reserve = [coin_qty] * n
 
         # number of token sorts in the pool
-        n = len(args)
         self.n = n
 
-        self.aInv = a
+        self.a_inv = a
 
         # transform our aInv into StableSwap's A
-        self.A = a*(n**n)
+        self.A = a * (n ** n)
 
-        D = 0
-        for x in args:
-            D += x
+        D = coin_qty * n
 
         # sum invariant
-        self.sumInv = D
+        self.sum_inv = D
 
         # product invariant
-        self.prodInv = (D/n) ** n
+        self.prod_inv = (D / n) ** n
 
     def poolsum(self):
         return sum(self.reserve)
@@ -35,9 +31,9 @@ class curvefi:
         return prod(self.reserve)
 
     def exchange(self, inamount, inindex: int, outindex: int):
-        a = self.aInv
-        D = self.sumInv
-        X = self.prodInv
+        a = self.a_inv
+        D = self.sum_inv
+        X = self.prod_inv
 
         # seperate input output assets from the rest of the pool
         in_out_assets = [self.reserve[i] for i in [inindex, outindex]]
@@ -58,10 +54,10 @@ class curvefi:
         prodexo = restprod * inres
 
         outres = (
-            (1-1/a)*D - sumexo + sqrt(
-                ((1-1/a)*D - sumexo)**2 + 4*D*X/a/prodexo
-            )
-        )/2
+            (1 - 1 / a) * D
+            - sumexo
+            + sqrt(((1 - 1 / a) * D - sumexo) ** 2 + 4 * D * X / a / prodexo)
+        ) / 2
 
         outamount = self.reserve[outindex] - outres
 
@@ -74,26 +70,42 @@ class curvefi:
         return outamount
 
 
-def inout(*args, inamount, inindex: int = 0, outindex: int = 1, a=80):
-    demoPool = curvefi(*args, a=a)
+def inout(coin_qty, n, inamount, inindex: int = 0, outindex: int = 1, a=80):
+    demoPool = curvefi(coin_qty, n, a=a)
     demoPool.exchange(inamount=inamount, inindex=inindex, outindex=outindex)
     return demoPool.reserve[inindex], demoPool.reserve[outindex]
 
 
-def plotinout(oneassetreserve, inrange=np.arange(-299, 699), inindex=0, outindex=1, a=80):
-    reservepair = [inout(oneassetreserve, oneassetreserve, oneassetreserve, inamount=x, inindex=0, outindex=outindex, a=a)
-                   for x in inrange]
+def plotinout(
+    coin_qty,
+    n,
+    inrange=np.arange(-299, 699),
+    inindex=0,
+    outindex=1,
+    a=80,
+):
+    reservepair = [
+        inout(
+            coin_qty,
+            n,
+            inamount=x,
+            inindex=0,
+            outindex=outindex,
+            a=a,
+        )
+        for x in inrange
+    ]
 
-    plt.plot(*zip(*reservepair), label=f'a={a}')
+    plt.plot(*zip(*reservepair), label=f"a={a}")
     plt.xlim(0, 700)
     plt.ylim(0, 700)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     oneassetreserve = 300
+    n = 2
 
     for a in [0.0001, 1, 10, 9999999]:
-        plotinout(oneassetreserve, a=a)
-    plt.scatter(oneassetreserve, oneassetreserve)
+        plotinout(oneassetreserve, n, a=a)
     plt.legend()
     plt.show()
