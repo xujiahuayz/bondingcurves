23e07597ad9c77944aa07e2116adbbab6b4ec288
naz <nazariy@inbox.ru>
2021-02-20 17:37:46 +0000

    [*] clean up

--- amms/curve/main.py
+++ amms/curve/main.py
@@ -1,28 +1,7 @@
-#!/usr/bin/env python
+from bondingcurves.amms.main import Amm
 
 
-def get_D(xp: list[int], amplification: float):
-    S = sum(xp)
-    if S == 0:
-        return 0
+class Curve(Amm):
+    def __init__(self, reserves: list[int], weights: list[float]):
+        super().__init__(reserves, weights)
 
-    n_coins = len(xp)
-    d_prev = 0.0
-    d = float(S)
-    Ann = amplification * n_coins
-
-    for i in range(255):
-        d_p = d
-        for x in xp:
-            d_p = d_p * d / (x * n_coins)
-        d_prev = d
-        d = (Ann * S + d_p * n_coins) * d / ((Ann - 1) * d + (n_coins + 1) * d_p)
-        print("d", d)
-        if abs(d_prev - d) <= 1:
-            break
-
-    return d
-
-
-if __name__ == "__main__":
-    get_D([300_442_800, 275_520_769, 230_782_523], 100)
--- amms/uniswap/main.py
+++ amms/uniswap/main.py
@@ -3,11 +3,11 @@ from bondingcurves.amms.balancer.main import Balancer
 
 class Uniswap(Balancer):
     def __init__(self, reserves: list[int]):
-        super().__init__(reserves, [0.5, 0.5])
+        uniswap_equal_weights = [0.5, 0.5]
+        super().__init__(reserves, uniswap_equal_weights)
 
 
 # if __name__ == "__main__":
 #     uniswap = Uniswap([1000, 1000])
 #     print(uniswap.divergence_loss(1.0, 0, 1))
 #     print(uniswap.divergence_loss(-0.5, 0, 1))
-
