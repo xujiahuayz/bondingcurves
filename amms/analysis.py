[+] balancer slippage formula corrected

amms/analysis.py
@@ -13,13 +13,13 @@ X2 = 1_000 * DP18
 
 class Analysis:
     def __init__(self):
-        self.balancer_95_5 = Balancer([X1, X2], [0.95, 0.05])
-        self.balancer_98_2 = Balancer([X1, X2], [0.98, 0.02])
+        self.balancer_5_95 = Balancer([X1, X2], [0.05, 0.95])
+        self.balancer_2_98 = Balancer([X1, X2], [0.02, 0.98])
         self.uniswap = Uniswap([X1, X2])
         self.curve_A_0 = Curve([X1, X2], 0)
         self.curve_A_400 = Curve([X1, X2], 400)
 
-        self.slippage_domain = np.arange(0.01 * X1, 0.51 * X1, 25 * DP18)
+        self.slippage_domain = np.arange(0.01 * X2, 0.51 * X2, 25 * DP18)
 
     def plot_amm_curve(self):
         pass
@@ -28,20 +28,20 @@ class Analysis:
     # 1 plot for all 3 on the same chart
 
     def plot_slippage(self):
-        slippage_balancer_95_5 = []
-        slippage_balancer_98_2 = []
+        slippage_balancer_5_95 = []
+        slippage_balancer_2_98 = []
         slippage_uniswap = []
         slippage_curve_A_0 = []
         slippage_curve_A_400 = []
 
-        for qty_in in self.slippage_domain:
-            slippage_balancer_95_5.append(self.balancer_95_5.slippage(qty_in, 0, 1))
-            slippage_balancer_98_2.append(self.balancer_98_2.slippage(qty_in, 0, 1))
-            slippage_uniswap.append(self.uniswap.slippage(qty_in, 0, 1))
-            slippage_curve_A_0.append(self.curve_A_0.slippage(qty_in, 0, 1))
-            slippage_curve_A_400.append(self.curve_A_400.slippage(qty_in, 0, 1))
+        slippage_domain = [x / X2 for x in self.slippage_domain]
 
-        plt.plot(self.slippage_domain, slippage_balancer_95_5)
+        for qty_in in self.slippage_domain:
+            slippage_balancer_5_95.append(self.balancer_5_95.slippage(qty_in, 1, 0))
+            slippage_balancer_2_98.append(self.balancer_2_98.slippage(qty_in, 1, 0))
+            slippage_uniswap.append(self.uniswap.slippage(qty_in, 1, 0))
+            slippage_curve_A_0.append(self.curve_A_0.slippage(qty_in, 1, 0))
+            slippage_curve_A_400.append(self.curve_A_400.slippage(qty_in, 1, 0))
 
         plt.show()
 