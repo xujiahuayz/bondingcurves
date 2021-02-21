867c633cc5797108d671bab98f00f2f676c231b2
naz <nazariy@inbox.ru>
2021-02-21 22:40:20 +0000

    [+] curve conservation function plot

--- amms/analysis.py
+++ amms/analysis.py
@@ -27,10 +27,31 @@ class Analysis:
         self.slippage_domain = np.arange(0.01 * X2, 0.27 * X2, 25 * DP18)
 
     def plot_amm_curve(self):
-        pass
+        balancer_95_5 = []
+        balancer_98_2 = []
+        uniswap = []
+        curve_A_1 = []
+        curve_A_400 = []
 
-    # 3 plots for the amm curve for each one
-    # 1 plot for all 3 on the same chart
+        domain = np.arange(-0.95 * X1, 5 * X1, 10 * DP18)
+        # trade and look at reserves
+
+        for x in domain:
+            curve_A_1.append(self.curve_A_1._compute_trade_qty_out(x, 0, 1))
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot([x[0] for x in curve_A_1], [x[1] for x in curve_A_1])
+        ax.set_xlabel(
+            "x", size=15,
+        )
+        ax.set_ylabel("y", size=15)
+        ax.set_title(r"Curve for $\mathcal{A}=1$", size=21)
+        # ax.xaxis.set_major_formatter(PercentFormatter(xmax=1.0))
+        # ax.yaxis.set_major_formatter(PercentFormatter(xmax=1.0))
+        fig.savefig(
+            os.path.join(FIGS_DIR, "conservation", "curve_A_1.pdf"), format="pdf"
+        )
 
     def plot_slippage(self):
         slippage_balancer_95_5_0in_1out = []
@@ -73,7 +94,10 @@ class Analysis:
         ax.set_title(r"Balancer 95%/5% ($w_1/w_2$)", size=21)
         ax.xaxis.set_major_formatter(PercentFormatter(xmax=1.0))
         ax.yaxis.set_major_formatter(PercentFormatter(xmax=1.0))
-        fig.savefig(os.path.join(FIGS_DIR, "balancer_95_5_0in_1out.pdf"), format="pdf")
+        fig.savefig(
+            os.path.join(FIGS_DIR, "slippage", "balancer_95_5_0in_1out.pdf"),
+            format="pdf",
+        )
 
         # Balancer 95-5, x2 in x1 out
         fig = plt.figure()
@@ -86,7 +110,10 @@ class Analysis:
         ax.set_title(r"Balancer 95%/5% ($w_1/w_2$)", size=21)
         ax.xaxis.set_major_formatter(PercentFormatter(xmax=1.0))
         ax.yaxis.set_major_formatter(PercentFormatter(xmax=1.0))
-        fig.savefig(os.path.join(FIGS_DIR, "balancer_95_5_1in_0out.pdf"), format="pdf")
+        fig.savefig(
+            os.path.join(FIGS_DIR, "slippage", "balancer_95_5_1in_0out.pdf"),
+            format="pdf",
+        )
 
         # Balancer 98-2, x1 in x2 out
         fig = plt.figure()
@@ -99,7 +126,10 @@ class Analysis:
         ax.set_title(r"Balancer 98%/2% ($w_1/w_2$)", size=21)
         ax.xaxis.set_major_formatter(PercentFormatter(xmax=1.0))
         ax.yaxis.set_major_formatter(PercentFormatter(xmax=1.0))
-        fig.savefig(os.path.join(FIGS_DIR, "balancer_98_2_0in_1out.pdf"), format="pdf")
+        fig.savefig(
+            os.path.join(FIGS_DIR, "slippage", "balancer_98_2_0in_1out.pdf"),
+            format="pdf",
+        )
 
         # Balancer 98-2, x2 in x1 out
         fig = plt.figure()
@@ -112,7 +142,10 @@ class Analysis:
         ax.set_title(r"Balancer 98%/2% ($w_1/w_2$)", size=21)
         ax.xaxis.set_major_formatter(PercentFormatter(xmax=1.0))
         ax.yaxis.set_major_formatter(PercentFormatter(xmax=1.0))
-        fig.savefig(os.path.join(FIGS_DIR, "balancer_98_2_1in_0out.pdf"), format="pdf")
+        fig.savefig(
+            os.path.join(FIGS_DIR, "slippage", "balancer_98_2_1in_0out.pdf"),
+            format="pdf",
+        )
 
         # Balancer 50-50
         fig = plt.figure()
@@ -125,7 +158,9 @@ class Analysis:
         ax.set_title(r"Balancer 50%/50% ($w_1/w_2$)", size=21)
         ax.xaxis.set_major_formatter(PercentFormatter(xmax=1.0))
         ax.yaxis.set_major_formatter(PercentFormatter(xmax=1.0))
-        fig.savefig(os.path.join(FIGS_DIR, "balancer_50_50.pdf"), format="pdf")
+        fig.savefig(
+            os.path.join(FIGS_DIR, "slippage", "balancer_50_50.pdf"), format="pdf"
+        )
 
         # Uniswap
         fig = plt.figure()
@@ -138,7 +173,7 @@ class Analysis:
         ax.set_title(r"Uniswap", size=21)
         ax.xaxis.set_major_formatter(PercentFormatter(xmax=1.0))
         ax.yaxis.set_major_formatter(PercentFormatter(xmax=1.0))
-        fig.savefig(os.path.join(FIGS_DIR, "uniswap.pdf"), format="pdf")
+        fig.savefig(os.path.join(FIGS_DIR, "slippage", "uniswap.pdf"), format="pdf")
 
         # Curve, A=1
         fig = plt.figure()
@@ -151,7 +186,7 @@ class Analysis:
         ax.set_title(r"Curve for $\mathcal{A}=1$", size=21)
         ax.xaxis.set_major_formatter(PercentFormatter(xmax=1.0))
         ax.yaxis.set_major_formatter(PercentFormatter(xmax=1.0, decimals=1))
-        fig.savefig(os.path.join(FIGS_DIR, "curve_A_1.pdf"), format="pdf")
+        fig.savefig(os.path.join(FIGS_DIR, "slippage", "curve_A_1.pdf"), format="pdf")
 
         # Curve, A=400
         fig = plt.figure()
@@ -164,19 +199,9 @@ class Analysis:
         ax.set_title(r"Curve for $\mathcal{A}=400$", size=21)
         ax.xaxis.set_major_formatter(PercentFormatter(xmax=1.0))
         ax.yaxis.set_major_formatter(PercentFormatter(xmax=1.0, decimals=2))
-        fig.savefig(os.path.join(FIGS_DIR, "curve_A_400.pdf"), format="pdf")
-
-    # 3 plots for the divergence loss (divergence loss vs. pct_change)
-    # 1 plot for all 3 on the same chart
-
-    def plot_divergence_loss(self):
-        pass
-
-    # 3 plots for slippage
-    # 1 plot for all 3 on the same chart
+        fig.savefig(os.path.join(FIGS_DIR, "slippage", "curve_A_400.pdf"), format="pdf")
 
 
 if __name__ == "__main__":
     analysis = Analysis()
-    analysis.plot_slippage()
-
+    analysis.plot_amm_curve()
