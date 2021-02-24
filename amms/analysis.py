f7a4bd1c244192c40e77a51cda32bc0d5d3f0f09
naz <nazariy@inbox.ru>
2021-02-24 19:26:44 +0000

    [+] conservation plot

--- amms/analysis.py
+++ amms/analysis.py
@@ -11,71 +11,65 @@ CURR_DIR = os.path.dirname(os.path.abspath(__file__))
 FIGS_DIR = os.path.join(CURR_DIR, "figures")
 
 DP18 = 1e18
-X1 = 1_000_000 * DP18
-X2 = 1_000_000 * DP18
+X1 = 1_000
+X2 = 1_000
+LABELPAD = 20
+FONTSIZE = 14
+LABELSIZE = 12
 
 
 class Analysis:
     def __init__(self):
-        self.balancer_95_5 = Balancer([X1, X2], [0.95, 0.05])
-        self.balancer_98_2 = Balancer([X1, X2], [0.98, 0.02])
-        self.balancer_60_40 = Balancer([X1, X2], [0.6, 0.4])
-        self.balancer_80_20 = Balancer([X1, X2], [0.8, 0.2])
-        self.balancer_50_50 = Balancer([X1, X2], [0.5, 0.5])
         self.uniswap = Uniswap([X1, X2])
+        self.balancer_95_5 = Balancer([X1, X2], [0.95, 0.05])
+        self.balancer_50_50 = Balancer([X1, X2], [0.5, 0.5])
+        self.balancer_5_95 = Balancer([X1, X2], [0.05, 0.95])
         self.curve_A_1 = Curve([X1, X2], 1)
         self.curve_A_5 = Curve([X1, X2], 5)
-        self.curve_A_10 = Curve([X1, X2], 10)
-        self.curve_A_20 = Curve([X1, X2], 20)
+        self.curve_A_50 = Curve([X1, X2], 50)
         self.curve_A_100 = Curve([X1, X2], 100)
-        self.curve_A_400 = Curve([X1, X2], 400)
-
-        self.slippage_domain = np.arange(0.0001 * X2, 0.27 * X2, 25 * DP18)
 
     def plot_amm_curve(self):
-        balancer_95_5 = []
-        balancer_98_2 = []
-        balancer_60_40 = []
-        balancer_80_20 = []
         balancer_50_50 = []
+        balancer_5_95 = []
+        balancer_95_5 = []
         uniswap = []
         curve_A_1 = []
         curve_A_5 = []
-        curve_A_10 = []
-        curve_A_20 = []
         curve_A_100 = []
 
-        domain = np.arange(-1 * X1, 4.9 * X1, 50 * DP18)
+        domain = np.arange(-1 * X1, 5.1 * X1, 25)
         # trade and look at reserves
 
         for x in domain:
-            balancer_95_5.append(self.balancer_95_5._compute_trade_qty_out(x, 0, 1))
-            balancer_98_2.append(self.balancer_98_2._compute_trade_qty_out(x, 0, 1))
-            balancer_50_50.append(self.balancer_50_50._compute_trade_qty_out(x, 0, 1))
-            balancer_60_40.append(self.balancer_60_40._compute_trade_qty_out(x, 0, 1))
-            balancer_80_20.append(self.balancer_80_20._compute_trade_qty_out(x, 0, 1))
             uniswap.append(self.uniswap._compute_trade_qty_out(x, 0, 1))
+            balancer_95_5.append(self.balancer_95_5._compute_trade_qty_out(x, 0, 1))
+            balancer_50_50.append(self.balancer_50_50._compute_trade_qty_out(x, 0, 1))
+            balancer_5_95.append(self.balancer_5_95._compute_trade_qty_out(x, 0, 1))
             curve_A_1.append(self.curve_A_1._compute_trade_qty_out(x, 0, 1))
             curve_A_5.append(self.curve_A_5._compute_trade_qty_out(x, 0, 1))
-            curve_A_10.append(self.curve_A_10._compute_trade_qty_out(x, 0, 1))
-            curve_A_20.append(self.curve_A_20._compute_trade_qty_out(x, 0, 1))
             curve_A_100.append(self.curve_A_100._compute_trade_qty_out(x, 0, 1))
 
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.plot([x[0] for x in curve_A_1], [x[1] for x in curve_A_1], linewidth=2)
         ax.plot([x[0] for x in curve_A_5], [x[1] for x in curve_A_5], linewidth=2)
-        ax.plot([x[0] for x in curve_A_10], [x[1] for x in curve_A_10])
-        ax.plot([x[0] for x in curve_A_100], [x[1] for x in curve_A_100])
+        ax.plot([x[0] for x in curve_A_100], [x[1] for x in curve_A_100], linewidth=2)
+        ax.tick_params(axis="x", labelsize=LABELSIZE)
+        ax.tick_params(axis="y", labelsize=LABELSIZE)
         ax.set_xlabel(
-            r"Pool's token #1 reserve, $r_1$", size=15,
+            r"Pool's token 1 reserve, $r_1$" + "\n" + r"$r_1 = 1000$",
+            labelpad=LABELPAD,
+            size=FONTSIZE,
         )
-        ax.set_ylabel(r"Pool's token #2 reserve, $r_2$", size=15)
-        ax.set_ylim([-1e23 / 2, 2.5e24])
-        ax.set_xlim([-1e23 / 2, 2.5e24])
-        # ax.axvline(ls="--")
-        # ax.axhline(ls="--")
-        ax.legend(["A=1", "A=5", "A=10", "A=100"])
+        # ax.set_ylabel(
+        #     r"Pool's token 2 reserve, $r_2$" + "\n" + r"$r_2 = 1000$",
+        #     labelpad=LABELPAD,
+        #     size=FONTSIZE,
+        # )
+        ax.set_ylim([-10, 2_010])
+        ax.set_xlim([-10, 2_010])
+        ax.legend(["1", "5", "100"], title=r"$\mathcal{A}$")
         fig.savefig(
             os.path.join(FIGS_DIR, "conservation", "conservation_curve.pdf"),
             format="pdf",
@@ -88,20 +82,26 @@ class Analysis:
             [x[0] for x in balancer_50_50], [x[1] for x in balancer_50_50], linewidth=2
         )
         ax.plot(
-            [x[0] for x in balancer_60_40], [x[1] for x in balancer_60_40], linewidth=2
+            [x[0] for x in balancer_5_95], [x[1] for x in balancer_5_95], linewidth=2
         )
-        ax.plot([x[0] for x in balancer_80_20], [x[1] for x in balancer_80_20])
-        ax.plot([x[0] for x in balancer_95_5], [x[1] for x in balancer_95_5])
-        ax.plot([x[0] for x in balancer_98_2], [x[1] for x in balancer_98_2])
+        ax.plot(
+            [x[0] for x in balancer_95_5], [x[1] for x in balancer_95_5], linewidth=2
+        )
+        ax.tick_params(axis="x", labelsize=LABELSIZE)
+        ax.tick_params(axis="y", labelsize=LABELSIZE)
         ax.set_xlabel(
-            r"Pool's token #1 reserve, $r_1$", size=15,
+            r"Pool's token 1 reserve, $r_1$" + "\n" + r"$r_1 = 1000$",
+            labelpad=LABELPAD,
+            size=FONTSIZE,
         )
-        ax.set_ylabel(r"Pool's token #2 reserve, $r_2$", size=15)
-        ax.set_ylim([-1e23 / 2, 2e24])
-        ax.set_xlim([0.45e24, 2e24])
-        # ax.axvline(ls="--", x=0.5e24)
-        # ax.axhline(ls="--")
-        ax.legend(["50%/50%", "60%/40%", "80%/20%", "95%/5%", "98%/2%"])
+        # ax.set_ylabel(
+        #     r"Pool's token 2 reserve, $r_2$" + "\n" + r"$r_2 = 1000$",
+        #     labelpad=LABELPAD,
+        #     size=FONTSIZE,
+        # )
+        ax.set_ylim([-10, 2_010])
+        ax.set_xlim([-10, 2_010])
+        ax.legend([r"$.5/.5$", r"$.5/.95$", r".95/.5"], title=r"$w_1/w_2$")
         fig.savefig(
             os.path.join(FIGS_DIR, "conservation", "conservation_balancer.pdf"),
             format="pdf",
@@ -111,15 +111,21 @@ class Analysis:
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.plot([x[0] for x in uniswap], [x[1] for x in uniswap], linewidth=2)
+        ax.tick_params(axis="x", labelsize=LABELSIZE)
+        ax.tick_params(axis="y", labelsize=LABELSIZE)
         ax.set_xlabel(
-            r"Pool's token #1 reserve, $r_1$", size=15,
+            r"Pool's token 1 reserve, $r_1$" + "\n" + r"$r_1 = 1000$",
+            labelpad=LABELPAD,
+            size=FONTSIZE,
         )
-        ax.set_ylabel(r"Pool's token #2 reserve, $r_2$", size=15)
-        ax.set_ylim([-1e23 / 2, 2e24])
-        ax.set_xlim([0.45e24, 2e24])
-        # ax.axvline(ls="--", x=0.5e24)
-        # ax.axhline(ls="--")
-        ax.legend(["Uniswap"])
+        ax.set_ylabel(
+            r"Pool's token 2 reserve, $r_2$" + "\n" + r"$r_2 = 1000$",
+            labelpad=LABELPAD,
+            size=FONTSIZE,
+        )
+        ax.set_ylim([-10, 2_010])
+        ax.set_xlim([-10, 2_010])
+        ax.legend(title="Uniswap")
         fig.savefig(
             os.path.join(FIGS_DIR, "conservation", "conservation_uniswap.pdf"),
             format="pdf",
@@ -129,16 +135,11 @@ class Analysis:
     def plot_slippage(self):
         slippage_balancer_95_5_0in_1out = []
         slippage_balancer_95_5_1in_0out = []
-        slippage_balancer_98_2_0in_1out = []
-        slippage_balancer_98_2_1in_0out = []
-        slippage_balancer_60_40_1in_0out = []
         slippage_balancer_50_50 = []
         slippage_uniswap = []
         slippage_curve_A_1 = []
-        curve_A_5 = []
-        curve_A_10 = []
+        slippage_curve_A_50 = []
         slippage_curve_A_100 = []
-        slippage_curve_A_400 = []
         uniswap = []
 
         slippage_domain = [x / X2 for x in self.slippage_domain]
@@ -150,38 +151,18 @@ class Analysis:
             slippage_balancer_95_5_1in_0out.append(
                 self.balancer_95_5.slippage(qty_in, 1, 0)
             )
-            slippage_balancer_98_2_0in_1out.append(
-                self.balancer_98_2.slippage(qty_in, 0, 1)
-            )
-            slippage_balancer_98_2_1in_0out.append(
-                self.balancer_98_2.slippage(qty_in, 1, 0)
-            )
-            slippage_balancer_60_40_1in_0out.append(
-                self.balancer_60_40.slippage(qty_in, 1, 0)
-            )
             slippage_balancer_50_50.append(self.balancer_50_50.slippage(qty_in, 0, 1))
             slippage_uniswap.append(self.uniswap.slippage(qty_in, 1, 0))
             slippage_curve_A_1.append(self.curve_A_1.slippage(qty_in, 1, 0))
-            curve_A_5.append(self.curve_A_5.slippage(qty_in, 1, 0))
+            slippage_curve_A_50.append(self.curve_A_50.slippage(qty_in, 1, 0))
             slippage_curve_A_100.append(self.curve_A_100.slippage(qty_in, 1, 0))
-            curve_A_10.append(self.curve_A_10.slippage(qty_in, 1, 0))
-            slippage_curve_A_400.append(self.curve_A_400.slippage(qty_in, 1, 0))
             uniswap.append(self.uniswap.slippage(qty_in, 0, 1))
 
         fig = plt.figure()
-        # plt.rcParams['savefig.facecolor'] = "0.8"
-        # plt.rcParams['figure.figsize'] = 4.5, 4.
-        # plt.rcParams['figure.max_open_warning'] = 50
         ax = fig.add_subplot(111)
         ax.plot(slippage_domain, [-y for y in slippage_balancer_50_50], linewidth=2)
         ax.plot(
-            slippage_domain, [-y for y in slippage_balancer_60_40_1in_0out], linewidth=2
-        )
-        ax.plot(
             slippage_domain, [-y for y in slippage_balancer_95_5_1in_0out], linewidth=2
-        )
-        ax.plot(
-            slippage_domain, [-y for y in slippage_balancer_98_2_1in_0out], linewidth=2
         )
         ax.set_xlabel(
             r"Trade size relative to reserve, $x_1 / r_1$", size=15,
@@ -192,7 +173,7 @@ class Analysis:
         ax.xaxis.set_major_formatter(PercentFormatter(xmax=1.0))
         ax.yaxis.set_major_formatter(PercentFormatter(xmax=1.0, decimals=1))
         ax.legend(
-            ["50%/50%", "60%/40%", "95%/5%", "98%/2%",]
+            ["50%/50%", "95%/5%", "5%/95%",]
         )
         fig.savefig(
             os.path.join(FIGS_DIR, "slippage", "slippage_balancer.pdf"),
@@ -221,16 +202,15 @@ class Analysis:
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.plot(slippage_domain, [-y for y in slippage_curve_A_1])
-        ax.plot(slippage_domain, [-y for y in curve_A_5])
-        ax.plot(slippage_domain, [-y for y in curve_A_10])
-        ax.plot(slippage_domain, [-y for y in slippage_curve_A_400])
+        ax.plot(slippage_domain, [-y for y in slippage_curve_A_50])
+        ax.plot(slippage_domain, [-y for y in slippage_curve_A_100])
         ax.set_xlabel(
             r"Trade size relative to reserve, $x_1 / r_1$", size=15,
         )
         ax.set_ylabel("slippage", size=15)
         ax.xaxis.set_major_formatter(PercentFormatter(xmax=1.0))
         ax.yaxis.set_major_formatter(PercentFormatter(xmax=1.0, decimals=1))
-        ax.legend(["A=1", "A=5", "A=10", "A=100"])
+        ax.legend(["A=1", "A=50", "A=100"])
         fig.savefig(
             os.path.join(FIGS_DIR, "slippage", "slippage_curve.pdf"),
             format="pdf",
@@ -243,9 +223,8 @@ class Analysis:
         curve_A_100 = []
 
         balancer_50_50 = []
-        balancer_60_40 = []
         balancer_95_5 = []
-        balancer_98_2 = []
+        balancer_5_95 = []
 
         uniswap = []
 
@@ -254,14 +233,12 @@ class Analysis:
 
         for qty in domain:
             curve_A_1.append(self.curve_A_1.divergence_loss(qty, 0, 1))
-            curve_A_10.append(self.curve_A_10.divergence_loss(qty, 0, 1))
             curve_A_100.append(self.curve_A_100.divergence_loss(qty, 0, 1))
 
         for pct_change in _domain:
             balancer_50_50.append(self.balancer_50_50.divergence_loss(pct_change, 1, 0))
-            balancer_60_40.append(self.balancer_60_40.divergence_loss(pct_change, 1, 0))
             balancer_95_5.append(self.balancer_95_5.divergence_loss(pct_change, 1, 0))
-            balancer_98_2.append(self.balancer_98_2.divergence_loss(pct_change, 1, 0))
+            balancer_5_95.append(self.balancer_5_95.divergence_loss(pct_change, 1, 0))
 
             uniswap.append(self.uniswap.divergence_loss(pct_change, 0, 1))
 
@@ -292,9 +269,7 @@ class Analysis:
         ax.yaxis.set_major_formatter(PercentFormatter(xmax=1.0, decimals=0))
         ax.xaxis.set_major_formatter(PercentFormatter(xmax=1.0, decimals=0))
         ax.plot(_domain, balancer_50_50, linewidth=2)
-        ax.plot(_domain, balancer_60_40, linewidth=2)
         ax.plot(_domain, balancer_95_5, linewidth=2)
-        ax.plot(_domain, balancer_98_2, linewidth=2)
         ax.legend(["50%/50%", "60%/40%", "95%/5%", "98%/2%"])
         fig.savefig(
             os.path.join(FIGS_DIR, "divergence_loss", "divloss_balancer.pdf"),
